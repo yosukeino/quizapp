a6172d82a871355d59ef70ab956aed91c26bd4a3
mbpmd2020 <mbp2020@MacBook-Pro.local>
2021-12-07 10:31:18 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+from quiz_brain import QuizBrain
+from question_model import Question
+from ui import QuizInterface
+
+response = requests.get('https://opentdb.com/api.php?amount=10&category=31&type=boolean')
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
+
+# while quiz.still_has_questions():
+#     quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
